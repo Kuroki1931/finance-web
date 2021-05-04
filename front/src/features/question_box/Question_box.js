14e4a1d72e4c146634d85c5d9adbe231dc70711a
kuroki <skuroki@fracta.ai>
2021-05-04 18:05:41 +0900

    m

--- front/src/features/question_box/Question_box.js
+++ front/src/features/question_box/Question_box.js
@@ -1,14 +1,34 @@
 import classes from './Question_box.module.css'
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { withCookies } from "react-cookie";
+import React, { useEffect } from 'react'
 
-const Question_box = () => {
+import {
+    selectQuestion_list,
+    fetchAsyncQuestion_listGet,
+    selectAsk_list,
+    fetchAsyncAsk_listGet,
+} from "./question_boxSlice";
+
+const Question_box = (props) => {
+    const dispatch = useDispatch();
+    const token = props.cookies.get("current-token");
+    const question_list = useSelector(selectQuestion_list)
+    const ask_list = useSelector(selectAsk_list)
+
+    useEffect(() => {
+        dispatch(fetchAsyncAsk_listGet(token))
+        dispatch(fetchAsyncQuestion_listGet())
+    }, [dispatch]);
+
     return (
         <div className={classes.all}>
             <div className={classes.head}>
                 質問箱
             </div>
+
         </div>
     )
 }
 
-export default Question_box
+export default withCookies(Question_box)
